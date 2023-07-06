5e24171317785c6f1e91f473e8c34b3f51f85179
Fet2113 <super.gjoll@yandex.ru>
2023-07-06 14:39:38 +0300

    ДЗ Collection.

---
+++ src/test/java/pro/sky/skyprospringhwCollection/SkyproSpringHwCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhwCollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringHwCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
